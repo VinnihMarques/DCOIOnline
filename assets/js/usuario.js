111dc824d30d659bc7f24002c132a18fd7fc9173
vinicius marques <vinicius marques>
2016-08-30 15:08:02 -0300

    Conexão com Bando de dados funcionando e inserção,alteração e consulta de usuários.

--- assets/js/usuario.js
+++ assets/js/usuario.js
@@ -5,10 +5,31 @@ app.controller('InserirUsuarioController',['$scope','$rootScope','$http','$locat
 	this.criar = function(){
 		$http.post('/usuario/create', this.usuario).success(function(resp){
 			console.log('Sucesso!');
-			$location.path('/Usuarios/listaUsuarios');
+			$location.path('/consultaUsuario/');
 
 		});
-};
+	};
+
+}]);
+
+app.controller('ConsultarUsuarioController',['$scope','$rootScope','$http','$location',function($scope,$rootScope,$http,$location){
+	$scope.thisUsuario = this;
+	this.usuario = {};
+	$scope.usuarios = [];
+
+	$http.get('/usuario/').success(function(resp){
+		$scope.usuarios = resp;
+	});
+
+	this.usuario = {};
+	this.criar = function(){
+		$http.post('/usuario/create', this.usuario).success(function(resp){
+			console.log('Sucesso!');
+			$location.path('/consultaUsuario');
+
+		});
+	};
+
 
 
 
